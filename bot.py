32711629da09eaec63f0db28bc574fd11d621752
inducedcandle17 <82191980+inducedcandle17@users.noreply.github.com>
2021-04-09 10:48:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,4 +18,4 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f"Hey! I am up and running with a ping of {round(client.latency*1000)}ms.")
 
-client.run('')   #Place token between the ''
+client.run('')   #Place token between the 'ODE2MjI3NzUxNzExMDgwNDg4.YD35VA.PKQm7upeaaVZM9in0P_UP2JqZG4'
